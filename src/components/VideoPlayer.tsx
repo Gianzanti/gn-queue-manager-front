7e5ada86afd4b0e686f0e3d941365114f2792026
Fabio Gianzanti <fabio.gianzanti@jbtec.com.br>
2024-08-17 14:56:39 -0300

    fix: mute video to allow autoplay

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -39,6 +39,7 @@ const VideoPlayer: FC<VideoPlayerProps> = ({ videoPath, onVideoEnd }) => {
                 // controls
                 onEnded={handleVideoEnd}
                 autoPlay
+                muted
                 style={{ display: isFullScreen ? 'block' : 'none' }}
             >
                 <source src={videoPath} type='video/mp4' />
